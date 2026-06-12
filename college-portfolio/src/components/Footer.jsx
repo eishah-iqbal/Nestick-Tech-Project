fbb9553685a4b10f1ac872c6671b788d5246a1fc
Eishah Iqbal <eishahiqbal@gmail.com>
2026-06-13 01:11:16 +0500

    made footer

---
+++ college-portfolio/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#111111] border-t mt-10 py-6 text-center text-gray-300">
+      © 2026 Nestick College. All rights reserved.
+    </footer>
+  );
+}
