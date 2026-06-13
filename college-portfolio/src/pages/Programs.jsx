e053737097ae14a08ea7a7496925bee72b88cafc
Eishah Iqbal <eishahiqbal@gmail.com>
2026-06-13 19:20:15 +0500

    worked on quick links

--- college-portfolio/src/pages/Programs.jsx
+++ college-portfolio/src/pages/Programs.jsx
@@ -1,10 +1,24 @@
 import programs from "../data/programs.json";
 import ProgramCard from "../components/ProgramCard";
 import { useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Programs() {
   const [searchParams] = useSearchParams();
   const selectedProgram = searchParams.get("program");
+
+  useEffect(() => {
+  if (selectedProgram) {
+    const element = document.getElementById(selectedProgram);
+
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  }
+}, [selectedProgram]);
 
   return (
     <div className="max-w-6xl mx-auto py-16 px-4">
@@ -12,9 +26,11 @@ export default function Programs() {
 
       <div className="grid md:grid-cols-2 gap-6">
         {programs.map((p) => (
+          <div id={p.name}>
           <ProgramCard key={p.id} program={p} 
           highlighted={selectedProgram === p.name}
           />
+        </div>
         ))}
       </div>
     </div>
